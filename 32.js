b5960715675748019e2b88bfb73d86089b4de1d1
Ruben Dominguez <a01568047@gmail.com>
2021-04-27 17:45:18 -0600

    32.- Crear un arreglo de 20 elementos con nombres de personas. Mostrar los elementos de la lista debiendo ir cada uno en una fila distinta.

---
+++ 32.js
@@ -0,0 +1,8 @@
+// 32.- Crear un arreglo de 20 elementos con nombres de personas.
+// Mostrar los elementos de la lista debiendo ir cada uno en una fila distinta.
+
+let nombres = ["Ruben", "Gabo", "PatitoGordito", "Tommy", "Carlos", "Juan",
+               "Cota", "Rodrigo", "Javier", "Ximena", "Maritrini", "Dali",
+               "Wong", "Melissa", "Edgar", "Ana", "Luis", "Pablo", "Meny"];
+
+nombres.forEach(nombre => console.log(nombre));
