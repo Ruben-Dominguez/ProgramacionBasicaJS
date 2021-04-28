40cae14b3b9ed0047eb179c8b90fd768e150e506
Ruben Dominguez <a01568047@gmail.com>
2021-04-27 21:55:51 -0600

    39.- Crear  una  tabla  de  3  paginas,  4  filas  y  5  columnas  donde  el  primer  elemento  valga  1,  el segundo 2, el tercero 3 y así sucesivamente.

---
+++ 39.js
@@ -0,0 +1,12 @@
+// 39.- Crear  una  tabla  de  3  paginas,  4  filas  y  5  columnas  donde  el
+// primer  elemento  valga  1,  el segundo 2, el tercero 3 y así sucesivamente.
+
+
+var numeros = [[1, 2, 3, 4, 5], [1, 2, 3, 4, 5], [1, 2, 3, 4, 5], [1, 2, 3, 4, 5]];
+console.table(numeros);
+
+var numeros2 = [[6, 7, 8, 9, 10], [6, 7, 8, 9, 10], [6, 7, 8, 9, 10], [6, 7, 8, 9, 10]];
+console.table(numeros2);
+
+var numeros3 = [[11, 12, 13, 14, 15], [11, 12, 13, 14, 15], [11, 12, 13, 14, 15], [11, 12, 13, 14, 15]];
+console.table(numeros3);
